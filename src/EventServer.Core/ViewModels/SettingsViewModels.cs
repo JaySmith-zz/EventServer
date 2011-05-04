c15ad3ae86ed27ed2b44de5949b7dd56d768d4cb
Jay Smith <jay@jaysmith.us>
2011-05-03 22:45:52 -0500

    Added Settings Location ViewModel

--- src/EventServer.Core/ViewModels/SettingsViewModels.cs
+++ src/EventServer.Core/ViewModels/SettingsViewModels.cs
@@ -13,6 +13,26 @@ namespace EventServer.Core.ViewModels
 
         [DisplayName("Number of tweets to display")]
         public int DisplayCount { get; set; }
-        
+    }
+
+    public class SettingsVenueViewModel
+    {
+        [DisplayName("Location")]
+        public string VenueName { get; set; }
+
+        [DisplayName("Location Phone")]
+        public string VenuePhone { get; set; }
+
+        [DisplayName("Address")]
+        public string VenueStreet { get; set; }
+
+        [DisplayName("City")]
+        public string VenueCity { get; set; }
+
+        [DisplayName("State")]
+        public string VenueState { get; set; }
+
+        [DisplayName("Postal Code")]
+        public string VenueZip { get; set; }
     }
 }
