71edf34b0554afa23d682edf1ae1841ab47d69fa
Tim Rayburn <tim@timrayburn.net>
2011-07-28 11:56:42 -0500

    Added Dallas TechFest times into the hard code Session times static. Blech.

--- src/EventServer.Core/Domain/Session.cs
+++ src/EventServer.Core/Domain/Session.cs
@@ -118,12 +118,12 @@ namespace EventServer.Core.Domain
         public static readonly IDictionary<string, string> TimeSlots = new Dictionary<string, string>
             {
                 {"None", "None"},
-                {"1", "08:50 AM - 09:50 AM"},
-                {"2", "10:00 AM - 11:00 AM"},
-                {"3", "11:10 AM - 12:10 PM"},
-                {"4", "01:30 PM - 02:30 PM"},
-                {"5", "02:40 PM - 03:40 PM"},
-                {"6", "03:50 PM - 04:50 PM"},
+                {"1", "09:00 AM - 10:15 AM"},
+                {"2", "10:30 AM - 11:45 AM"},
+                {"3", "12:45 PM - 02:00 PM"},
+                {"4", "02:15 PM - 03:30 PM"},
+                {"5", "03:45 PM - 05:00 PM"},
+                {"6", "05:15 PM - 06:00 PM"},
             };
 
     }
