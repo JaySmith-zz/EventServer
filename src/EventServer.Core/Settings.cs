31f320722964d8e6ac4e052d06bcd9cf8668b9ee
Jay Smith <jay@jaysmith.us>
2011-08-03 15:52:01 -0500

    Issue #3: modified Settings.cs to be thread safe

--- src/EventServer.Core/Settings.cs
+++ src/EventServer.Core/Settings.cs
@@ -1,31 +1,48 @@
-using System;
 using EventServer.Core.Domain;
 
 namespace EventServer.Core
 {
-    public static class Settings
+    public class Settings
     {
         private const int DefaultId = 1;
+        private static readonly object mutex = new object();
+        private static volatile AppSettings instance;
 
-        static Settings()
+        public static AppSettings Instance
         {
-            Instance = LoadSettings();
+            get
+            {
+                if (instance == null)
+                {
+                    lock (mutex)
+                    {
+                        if (instance == null)
+                        {
+                            instance = LoadSettings();
+                        }
+                    }
+                }
+
+                return instance;
+            }
         }
 
-        public static AppSettings Instance { get; private set; }
+        public static void SaveSettings(AppSettings settings)
+        {
+            lock (mutex)
+            {
+                instance = settings;
+                Ioc.Resolve<IRepository>().Save(settings);
+            }
+
+        }
 
         private static AppSettings LoadSettings()
         {
             var repository = Ioc.Resolve<IRepository>();
 
             return repository.Get<AppSettings>(DefaultId) ?? repository.Save(new AppSettings());
-        }
 
-        public static void SaveSettings(AppSettings settings)
-        {
-            Instance = settings;
-
-            Ioc.Resolve<IRepository>().Save<AppSettings>(settings);
         }
     }
 }
